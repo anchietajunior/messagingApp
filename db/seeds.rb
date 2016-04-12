99801a689bb812db6e4e75388c360340769d20cd
anchietajunior <santosjr87@gmail.com>
2016-04-12 17:49:00 -0300

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create([{ email: 'kennedy@email.com', first_name: 'Kennedy',
+last_name: 'Noia', password: '12345678', password_confirmation: '12345678'
+},
+{ email: 'santosjr87@gmail.com', first_name: 'Anchieta',
+last_name: 'Junior', password: '12345678', password_confirmation: '12345678'}
+])
